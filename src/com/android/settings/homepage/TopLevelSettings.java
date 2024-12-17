5e3c43cb88196fd6e461855cad4ec4f24196d9cd
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2024-12-17 05:21:35 +0000

    Redesign Dashboard UI Based on A15 design Change-Id: I48f778a88175357c18d4ccbeb88d99ece8048201 Signed-off-by: Mohammad Hasan Keramat J <ikeramat@libremobileos.com>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -89,7 +89,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     @Override
     protected int getPreferenceScreenResId() {
-        return Flags.homepageRevamp() ? R.xml.top_level_settings_v2 : R.xml.top_level_settings;
+        return Flags.homepageRevamp() ? R.xml.mist_top_level_settings : R.xml.top_level_settings;
     }
 
     @Override
@@ -395,7 +395,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(
                     Flags.homepageRevamp()
-                            ? R.xml.top_level_settings_v2
+                            ? R.xml.mist_top_level_settings
                             : R.xml.top_level_settings) {
 
                 @Override
